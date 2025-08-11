c3853cf4e525b24208e85ebaf3f84d48a37956e7
cedoe70 <cedoe70@gmail.com>
2025-08-11 05:08:21 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
